80fa116d6f8c9801026ddb6c48925cf9ab215f75
Theo Bonnard <theo.bonnard@supelec.fr>
2020-04-16 20:36:04 +0200

    Name every gradient

--- src/color.ts
+++ src/color.ts
@@ -64,32 +64,46 @@ function bilinearReg(point: Position, f00: number, f10: number, f01: number, f11
     return value;
 }
 
-const gColor1: GradientColors = {
+const gRainbow: GradientColors = {
     topLeftColor: { red: 239, green: 252, blue: 84 },
     topRightColor: { red: 120, green: 239, blue: 197 },
     bottomRigthColor: { red: 74, green: 69, blue: 215 },
     bottomLeftColor: { red: 235, green: 66, blue: 205 },
 }
 
-const gColor2: GradientColors = {
+const gFadedDream: GradientColors = {
     topLeftColor: { red: 70, green: 239, blue: 200 },
     topRightColor: { red: 216, green: 224, blue: 227 },
     bottomRigthColor: { red: 235, green: 90, blue: 193 },
     bottomLeftColor: { red: 91, green: 57, blue: 195 },
 }
 
-const gColor3: GradientColors = {
+const gDarkRainbow: GradientColors = {
     topLeftColor: { red: 232, green: 67, blue: 65 },
     topRightColor: { red: 28, green: 48, blue: 52 },
     bottomRigthColor: { red: 0, green: 221, blue: 229 },
     bottomLeftColor: { red: 211, green: 235, blue: 115 },
 }
 
-const gColor4: GradientColors = {
+const gEspresso: GradientColors = {
     topLeftColor: { red: 241, green: 216, blue: 245 },
     topRightColor: { red: 98, green: 90, blue: 255 },
     bottomRigthColor: { red: 154, green: 50, blue: 103 },
     bottomLeftColor: { red: 194, green: 109, blue: 44 },
+}
+
+const gSunset: GradientColors = {
+    topLeftColor: { red: 15, green: 29, blue: 38 },
+    topRightColor: { red: 106, green: 115, blue: 102 },
+    bottomRigthColor: { red: 89, green: 57, blue: 50 },
+    bottomLeftColor: { red: 217, green: 129, blue: 78 },
+}
+
+const gCreamCoffee: GradientColors = {
+    topLeftColor: { red: 235, green: 238, blue: 242 },      // white
+    topRightColor: { red: 237, green: 190, blue: 149 },     // cream
+    bottomRigthColor: { red: 13, green: 0, blue: 0 },       // black
+    bottomLeftColor: { red: 115, green: 47, blue: 23 },     // caramel
 }
 
 export function randomGradientColor(): GradientColors {
@@ -97,5 +111,10 @@ export function randomGradientColor(): GradientColors {
 }
 
 export const gColors: GradientColors[] = [
-    gColor1, gColor2, gColor3, gColor4
+    gRainbow,
+    gFadedDream,
+    gDarkRainbow,
+    gEspresso,
+    gSunset,
+    gCreamCoffee,
 ];
